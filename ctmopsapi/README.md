eed61032e2e384c269f5e89697f0a1573bf82251
ncullum <ncullum@unallocated.barefruit.co.uk>
2019-12-16 06:59:38 +0000

    New ctmopsapi AJF interaction

---
+++ ctmopsapi/README.md
@@ -0,0 +1,26 @@
+ 
+
+## CTMAPI Production environment interaction (AJF)
+
+### Introduction
+
+The Control-M automation api has many use cases. The demand for dynamic interaction with scheduling has never been greater.
+As the aapi matures, the ability to support and manage these dynamic requirements is becoming stronger and stronger.
+This project works with examples of interacting with the Control-M production environment, also known as the AJF.
+
+
+#### Disclaimer
+
+This application is not linked to or based on any particular business at all.
+The contents are all fictional and have been fabricated for demonstration purposes only.
+
+
+## What's in it?
+
+
+## Requirements
+
+1.	Control-M V19 or higher
+2.	Automation api access 
+
+
---
+++ ctmopsapi/dynamops4.py
@@ -0,0 +1,71 @@
+
+import os
+import sys
+import json
+import collections
+import codecs
+import pprint
+from matplotlib.cbook import unique
+import subprocess
+import commands
+
+                #Get the status of the jobs from specified search criteria and write to file
+
+def datarefresh ():
+    
+    jobfolder = 'ncu-ajfmanip'
+    os.system(('ctm run jobs:status::get -s "ctm=ctmaws&folder=%s" > /Users/ncullum/eclipse-workspace/ctmopsapi/myout.json')%(jobfolder)) 
+    #os.system(('ctm run jobs:status::get -s "ctm=ctmaws&folder=%s"')%(jobfolder))
+
+#datarefresh()
+
+                #Check whether there is already a data flow running, if not, rerun the flow
+def getinline ():
+    
+    with open('/Users/ncullum/eclipse-workspace/ctmopsapi/myout.json') as json_file:
+        
+        jobfolder = 'ncu-ajfmanip'
+        
+        #mydata = commands.getoutput('ctm run jobs:status::get -s "ctm=ctmaws&folder=ncu-ajfmanip"')
+        mydata = json.loads(json_file.read())
+        #mydata = mydata.decode("utf-8")
+        #pprint.pprint(mydata)
+        #json_file = os.system ('ctm run jobs:status::get -s "ctm=ctmaws&folder=ncu-ajfmanip"')
+        jobid = ""
+        timecalc = 0
+        jobstatus = ""
+        for uniqid in mydata['statuses']:
+            if uniqid['name'] == "ncu-apiajf1" and timecalc < int(uniqid["startTime"]): #and uniqid['status'] == "Ended OK":
+                jobid = uniqid['jobId']
+                timecalc = int(uniqid["startTime"])
+                folder = uniqid['folder']
+                jobstatus = uniqid['status']
+                #print (jobstatus)
+                #print (jobid)
+                if jobstatus == "Ended OK":
+                    print("%s %s , triggering rerun")%(jobid,jobstatus)
+                    os.system(('ctm run job::rerun %s')%(jobid))
+                    print(uniqid['jobId'])
+                    runcount = int(uniqid['numberOfRuns'])
+                    print("Started run number %s")%(runcount+01)
+                    jname = uniqid['name']
+                    os.system(('ctm run job:output::get %s > /Users/ncullum/eclipse-workspace/ctmopsapi/oparchive/%s'+'-run-%s.txt' )%(jobid,jname,runcount))
+            
+                else :
+                        runcount = uniqid['numberOfRuns']
+                        print("Job %s Status is still %s Cannot run now")%(jobid,jobstatus)
+                        print("Run number %s in progress")%(runcount)
+                
+            for jobs in mydata['statuses']:
+                jobs = jobs
+                #print(uniqid['jobId'])
+                #print(folder)
+                #print(jobs)
+                #
+            else :
+                exit
+        
+        #os.system(('ctm run job::free %s')%(myvar))
+    
+getinline()
+
